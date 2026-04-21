70c0585abce448d35885be03a30f27ac5360da81
Rajendhiran E <rajendhiran.easu@liveramp.com>
2026-04-21 12:00:54 +0530

    updated to fastapi standard

--- app/practice/main.py
+++ app/practice/main.py
@@ -19,7 +19,9 @@ app = FastAPI(
 app.include_router(user_router, prefix=API_V1_PREFIX)
 app.include_router(product_router, prefix=API_V1_PREFIX)
 
-if __name__ == "__main__":
-    import uvicorn
+# commented below and using `fastapi dev app/practice/main.py` using fastapi[standard]
 
-    uvicorn.run("app.practice.main:app", host="127.0.0.1", port=8000, reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#
+#     uvicorn.run("app.practice.main:app", host="127.0.0.1", port=8000, reload=True)
