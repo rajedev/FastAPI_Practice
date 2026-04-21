389b1e8afde87394548138e9f08a560c1a1bda1d
Rajendhiran E <rajendhiran.easu@liveramp.com>
2026-04-21 18:59:18 +0530

    form data - form, query and path param validation handling

--- app/practice/router/user_route.py
+++ app/practice/router/user_route.py
@@ -4,7 +4,7 @@ Date: 18/04/26
 Description: Simple Userinfo API methods
 """
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Form
 
 user_router = APIRouter(prefix="/users", tags=["users"])
 
@@ -19,3 +19,14 @@ def initial():
 @user_router.get("/{uid}")
 def user(uid: int):
     return {"is_user_active": uid in user_id}
+
+
+@user_router.post(path="/raise_issue")
+def raise_issue(uid: int = Form(...),
+                  issue_description: str = Form(..., min_length=5, max_length=50)):
+    is_user_exist = uid in user_id
+    if is_user_exist:
+        return {
+            "message": f"Issue submitted by {uid} and Details:{issue_description}"
+        }
+    raise HTTPException(status_code=404, detail="User not exist")
